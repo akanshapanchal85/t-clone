54850751659cfca0980537fc998b735a4e5649eb
Akansha <pakansha914@gmail.com>
2022-04-20 21:18:48 +0530

    tinder-card install

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Header from './components/Header'
+import TinderCard from './components/TinderCard';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const App = () => {
   return (
-
-    <div>
+    <div className='App'>
     <Header/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TinderCard/>} />
+      </Routes>
+    </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/TinderCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TinderCard = () => {
+  return (
+    <div>
+        <h1>Tinder Cards</h1>
+    </div>
+  )
+}
+
+export default TinderCard
